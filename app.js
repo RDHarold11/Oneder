dd69c4fbd1ea0c86fe112a1c9318df06222411ec
RDHarold11 <74198960+RDHarold11@users.noreply.github.com>
2022-06-18 21:02:10 -0400

    Add files via upload
    Probando los archivos.

---
+++ app.js
@@ -0,0 +1,17 @@
+document.querySelector('.menu__btn').addEventListener('click', () =>{
+    document.querySelector('.class__items').classList.toggle('show')
+});
+
+
+/* No va con el menu responsive */
+/* ScrollReveal().reveal('.header');
+ScrollReveal().reveal('.content-section', {delay: 500});
+ScrollReveal().reveal('.team', {delay: 500});
+ScrollReveal().reveal('.people', {delay: 600});
+ScrollReveal().reveal('.space', {delay:600});
+ScrollReveal().reveal('.porfolio', {delay:500});
+ScrollReveal().reveal('.column', {delay:600});
+ScrollReveal().reveal('.card', {delay: 600});
+ScrollReveal().reveal('.testimonials', {delay:500});
+ScrollReveal().reveal('.prices_card', {delay:500});
+ScrollReveal().reveal('.questions', {delay:500}); */
